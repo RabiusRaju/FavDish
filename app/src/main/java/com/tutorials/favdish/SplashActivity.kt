e7ddadc3cd3ecf2fe01b4dfdc123955a5e073b02
raju <rabiussani8121@gmail.com>
2022-06-09 17:57:33 +0600

    first commit

---
+++ app/src/main/java/com/tutorials/favdish/SplashActivity.kt
@@ -0,0 +1,35 @@
+package com.tutorials.favdish
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import com.tutorials.favdish.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+
+    private var splashBinding : ActivitySplashBinding? = null;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        splashBinding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(splashBinding!!.root)
+        init()
+    }
+
+    private fun init(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        }else{
+            @Suppress("DEPRECATION")
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+
+        //splashBinding!!.tvAppName.text = "Hello World"
+    }
+}
